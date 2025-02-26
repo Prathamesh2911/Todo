33d2a7266baafa508aea7e83aaee3df65f503391
Prathamesh Shinde <98965952+Prathamesh2911@users.noreply.github.com>
2025-02-26 15:12:03 +0530

    Update tasks_screen.dart
    Use Consumer here to rebuild task count when it changes

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,72 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/module/task_data.dart';
 import 'package:todo/widgets/tasks.list.dart';
-import 'package:flutter/material.dart';
 import 'package:todo/screens/add_task_screen.dart';
-import 'package:provider/provider.dart';
 
 class TaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.lightBlueAccent,
-          child: Icon(Icons.add),
-          onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (context) => AddTaskScreen(),
-            );
-          },
-        ),
-        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(
-            padding: const EdgeInsets.only(
-                top: 60.0, left: 30, right: 30, bottom: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 30.0,
-                  child: Icon(
-                    Icons.list,
-                    size: 30.0,
-                    color: Colors.lightBlueAccent,
-                  ),
+        child: Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => AddTaskScreen(),
+          );
+        },
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          padding: const EdgeInsets.only(top: 60.0, left: 30, right: 30, bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 30.0,
+                child: Icon(
+                  Icons.list,
+                  size: 30.0,
+                  color: Colors.lightBlueAccent,
                 ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  'Todo',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                Text(
-                  '${Provider.of<TaskData>(context, listen: false).taskCount} Tasks',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              decoration: BoxDecoration(
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Todo',
+                style: TextStyle(
                   color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20.0),
-                      topRight: Radius.circular(20.0))),
-              child: TasksList(),
-            ),
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              // Use Consumer here to rebuild task count when it changes
+              Consumer<TaskData>(
+                builder: (context, taskData, child) {
+                  return Text(
+                    '${taskData.taskCount} Tasks',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        ]));
+        ),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0))),
+            child: TasksList(),
+          ),
+        ),
+      ]),
+    );
   }
 }
